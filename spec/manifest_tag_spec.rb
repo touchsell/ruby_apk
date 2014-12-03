b141688bb768b3b1ef8f79605b710aa90a35931f
Guillaume Mulocher <guillaume@nimbledroid.com>
2014-12-03 18:03:31 -0500

    wip - added spec files

---
+++ spec/manifest_tag_spec.rb
@@ -0,0 +1,25 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe "ManifestTag" do
+    # describe "#package" do
+      # subject { manifest.package}
+      # it { should == "example.app.sample" }
+    # end
+    # describe "#version_code" do
+      # subject { manifest.version_code}
+      # it { should == 101 }
+    # end
+    # describe "#version_name" do
+      # subject { manifest.version_name}
+      # it { should == "1.0.1-malware2" }
+    # end
+    # describe "#min_sdk_version" do
+      # subject { manifest.min_sdk_version}
+      # it { should == 10 }
+    # end
+    # describe "#target_sdk_version" do
+      # subject { manifest.target_sdk_version}
+      # it { should == 10 }
+    # end
+end
+
